dd018f4b467f0b060c93e26ec5a675b5dab6f599
Afroz Ansari <106192041+afrozansarigithub@users.noreply.github.com>
2024-01-24 18:26:20 +0000

    cgg

--- src/component/Todolist.jsx
+++ src/component/Todolist.jsx
@@ -1,48 +1,57 @@
-import React, { useState } from 'react'
-
-const Todolist = () => {
-  const details=[
-    {
-      id:1,
-      text:'eight Academy',
-      completed:true
-    },
-    {
-      id:2,
-      text:'computer education',
-      completed:false
-    },
-    {
-      id:3,
-      text:'school management',
-      completed:true
-    },
-  ];
-
-  const[task,setTask]=useState([details]);
-  const[text,setText]=useState('');
-
-  let addtask=(text)=>{
-    const newTask={
-      id:Date.now(),
-      text,
-      completed:false
-    }
-    setTask([...task,newTask]);
-    setTask('');
-
-
+import React from "react";
+import Todoitem from "./TodoItem";
+const Todolist=()=>{
+  const [tasks, setTasks] = useState([
+  {
+  id: 1,
+  text: 'Doctor Appointment',
+  completed: true
+  },
+  {
+  id: 2,
+  text: 'Meeting at School',
+  completed: false
   }
-  return (
-    <div>
-      <h1>todo list</h1>
-      <input type="text"
-      value={text}
-      onChange={(e)=>setText(e.target.value)}
-      />
-      <button onClick={()=>addtask}>Add</button>
-    </div>
-  )
-}
-
-export default Todolist
+  ]);
+  
+  const [text, setText] = useState('');
+ function addTask(text) {
+  const newTask = {
+  id: Date.now(),
+  text,
+  completed: false
+  };
+  setTasks([...tasks, newTask]);
+  setText('');
+  }
+ function deleteTask(id) {
+  setTasks(tasks.filter(task => task.id !== id));
+  }
+ function toggleCompleted(id) {
+  setTasks(tasks.map(task => {
+  if (task.id === id) {
+  return {...task, completed: !task.completed};
+  } else {
+  return task;
+  } 
+  }));
+  }
+ return (
+  <div className="todo-list">
+  {tasks.map(task => (
+  <TodoItem
+  key={task.id} 
+  task={task}
+  deleteTask={deleteTask}
+  toggleCompleted={toggleCompleted} 
+  />
+  ))}
+ <input
+  value={text}
+  onChange={e => setText(e.target.value)} 
+  />
+ <button onClick={() => addTask(text)}>Add</button>
+  </div>
+  );
+ }
+ export default Todolist;
