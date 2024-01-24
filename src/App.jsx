7b94f15aef0b7123fe688fd328e46992f8026472
Afroz Ansari <106192041+afrozansarigithub@users.noreply.github.com>
2024-01-24 20:16:32 +0000

    del

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,9 @@ import React from 'react'
 const App = () => {
   return (
     <div>
-      
+          <h1 className="text-3xl font-bold underline">
+      Hello world!
+    </h1>
       {/* <FetchApi/> */}
       {/* <Pushdata/> */}
       {/* <Todolist/> */}
