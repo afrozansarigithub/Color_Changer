718c1f4c6835cc2097b70e9770a4fba1f06271aa
Afroz Ansari <106192041+afrozansarigithub@users.noreply.github.com>
2024-01-24 20:03:42 +0000

    dfsf

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 // import FetchApi from './component/FetchApi'
-import Practice from './component/Practice'
+
 // import Color from './Color'
 // import Pushdata from './component/Pushdata'
 // import Todolist from './component/Todolist'
@@ -8,7 +8,7 @@ import Practice from './component/Practice'
 const App = () => {
   return (
     <div>
-      <Practice/>
+      
       {/* <FetchApi/> */}
       {/* <Pushdata/> */}
       {/* <Todolist/> */}
